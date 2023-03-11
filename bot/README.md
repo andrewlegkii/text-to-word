16ace38cfe6573ed28a8a58c4708a8190a817ca5
andrewlegkii <legkiy.andrey@bk.ru>
2023-03-11 23:31:47 +0300

    app

---
+++ bot/README.md
@@ -0,0 +1,13 @@
+# Text-to-Word Telegram Bot
+
+This is a Telegram bot that can convert text messages into Word documents.
+
+## Installation
+
+To run this bot, you will need to have Python 3 and the following Python libraries installed:
+
+- python-telegram-bot
+- python-docx
+
+You can install the required libraries using pip:
+
---
+++ bot/main.py
@@ -0,0 +1,38 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import docx
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Добро пожаловать в бот Text-to-Word! Пожалуйста, отправьте мне текст, который вы хотите преобразовать.")
+
+def convert_text_to_word(update, context):
+    # Получаем текстовое сообщение от пользователя
+    text = update.message.text
+
+    # Создаем новый документ Word
+    document = docx.Document()
+
+    # Вставляем преобразованный текст в документ Word
+    document.add_paragraph(text)
+
+    # Сохраняем документ Word как файл
+    document.save('output.docx')
+
+    # Отправляем документ Word обратно пользователю
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open('output.docx', 'rb'))
+
+def main():
+    # Создаем Telegram-бота и добавляем обработчики команд и сообщений
+    updater = Updater(token='5978160141:AAEv-aR_YApWY0cKKxepNBKyCLwkIfqyQuA', use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    convert_handler = MessageHandler(Filters.text & ~Filters.command, convert_text_to_word)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(convert_handler)
+
+    # Запускаем бота
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
